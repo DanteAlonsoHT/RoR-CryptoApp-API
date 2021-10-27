ee15ad67afe12933cb2566471ebde9820a1c2e1f
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-26 20:41:30 -0500

    Importar Bootstrap y Styles.scss en archivo JavaScript principal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'boostrap'
+import 'css/styles'
 
 Rails.start()
 Turbolinks.start()
