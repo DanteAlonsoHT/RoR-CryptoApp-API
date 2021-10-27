2892789f5a4984f6e096cd38abdcc417ebd93388
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-27 18:45:21 -0500

    Generar las funciones para separar la lógica y extraer los precios individuales

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+    def bitcoin_price
+        @crypto_prices ? @crypto_prices[:bitcoin_price] : "loading"
+    end
+
+    def ethereum_price
+        @crypto_prices ? @crypto_prices[:ethereum_price] : "loading"
+    end
+
+    def cardano_price
+        @crypto_prices ? @crypto_prices[:cardano_price] : "loading"
+    end
 end
