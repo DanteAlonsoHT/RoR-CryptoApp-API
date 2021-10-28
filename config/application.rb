bcdd889451cec55ee785acaeb895088f9cf3f967
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-28 12:33:27 -0500

    Arreglar código Ruby de acuerdo a Rubocop y Agregar ruta Post para exportar

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'csv'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
