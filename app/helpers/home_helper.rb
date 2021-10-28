84f9bca07da17e40cef7d42a3b3d748ed837457c
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-28 16:31:42 -0500

    Agregar comentarios para documentar el Home Helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,12 @@
 module HomeHelper
+  ##
+  # Método 'bitcoin_price' obtiene el precio de Bitcoin desde el Home Controller
+  # usando la API de Messuri.
+  #
+  # @params { Hash } crypto_prices -> Hash con los valores de las criptomonedas
+  #
+  # @return { String } Valor de Bitcoin o 'loading'
+
   def bitcoin_price(crypto_prices = @crypto_prices)
     if (crypto_prices).is_a?(Hash)
       if (crypto_prices[:bitcoin_price].is_a? Numeric) && crypto_prices[:bitcoin_price].to_s.length < 12
@@ -11,6 +19,14 @@ module HomeHelper
     end
   end
 
+  ##
+  # Método 'ethereum_price' obtiene el precio de Ethereum desde el Home Controller
+  # usando la API de Messuri.
+  #
+  # @params { Hash } crypto_prices -> Hash con los valores de las criptomonedas
+  #
+  # @return { String } Valor de Ethereum o 'loading'
+
   def ethereum_price(crypto_prices = @crypto_prices)
     if (crypto_prices).is_a?(Hash)
       if (crypto_prices[:ethereum_price].is_a? Numeric) && crypto_prices[:ethereum_price].to_s.length < 12
@@ -22,6 +38,14 @@ module HomeHelper
       'loading'
     end
   end
+
+  ##
+  # Método 'cardano_price' obtiene el precio de Cardano desde el Home Controller
+  # usando la API de Messuri.
+  #
+  # @params { Hash } crypto_prices -> Hash con los valores de las criptomonedas
+  #
+  # @return { String } Valor de Cardano o 'loading'
 
   def cardano_price(crypto_prices = @crypto_prices)
     if (crypto_prices).is_a?(Hash)
