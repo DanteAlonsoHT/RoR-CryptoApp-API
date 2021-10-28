34befec000af935c875f2c73bf3aeeab3258486e
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-28 12:42:16 -0500

    Agregar helpers para resumir lógica al manejar datos de Messari API

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,37 @@
 module HomeHelper
-    def bitcoin_price
-        @crypto_prices ? @crypto_prices[:bitcoin_price] : "loading"
+  def bitcoin_price(crypto_prices = @crypto_prices)
+    if (crypto_prices).is_a?(Hash)
+      if (crypto_prices[:bitcoin_price].is_a? Numeric) && crypto_prices[:bitcoin_price].to_s.length < 12
+        crypto_prices[:bitcoin_price]
+      else
+        'loading'
+      end
+    else
+      'loading'
     end
+  end
 
-    def ethereum_price
-        @crypto_prices ? @crypto_prices[:ethereum_price] : "loading"
+  def ethereum_price(crypto_prices = @crypto_prices)
+    if (crypto_prices).is_a?(Hash)
+      if (crypto_prices[:ethereum_price].is_a? Numeric) && crypto_prices[:ethereum_price].to_s.length < 12
+        crypto_prices[:ethereum_price]
+      else
+        'loading'
+      end
+    else
+      'loading'
     end
+  end
 
-    def cardano_price
-        @crypto_prices ? @crypto_prices[:cardano_price] : "loading"
+  def cardano_price(crypto_prices = @crypto_prices)
+    if (crypto_prices).is_a?(Hash)
+      if (crypto_prices[:cardano_price].is_a? Numeric) && crypto_prices[:cardano_price].to_s.length < 12
+        crypto_prices[:cardano_price]
+      else
+        'loading'
+      end
+    else
+      'loading'
     end
+  end
 end
